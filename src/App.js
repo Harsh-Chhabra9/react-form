fc1fa24622c031cdf37302d1a7d94ec30d804dda
harsh chhabra <harshchhabra199@gmail.com>
2021-03-19 13:40:57 +0530

    change working

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component{
+  // this.handleAdd = this.handleChange.bind(this) 
+  //   this.handleSubmit = this.handleSubmit.bind(this) 
   state={
-    name:"",
+    name1:"",
     email:"",
-    password:""
+    password:"",
   }
+  
   handleAdd=async e=>{
-    await this.setState({
-      name:e.target.value,
-      email:e.target.value,
-      password:e.target.value,
+    console.log(e.target.name); 
+    this.setState({
+       
+      [e.target.name]:e.target.value
     })
+    
   }
   handleSubmit= e =>{
     e.preventDefault();
-    console.log(this.state.name);
+    console.log(this.state.name1);
+    console.log(this.state.email);
+    console.log(this.state.password);
     let formData =new FormData();
     formData.append("name",this.state.name);
     formData.append("email",this.state.email);
@@ -35,15 +41,15 @@ class App extends Component{
       <h1>Registration</h1>
           <div className="mb-3">
               <label htmlFor="name" className="form-label">Name</label>
-              <input type="text"   className="form-control" id="name" placeholder="Name" />
+              <input type="text" onChange={this.handleAdd} name="name1" className="form-control" id="name" placeholder="Name"/>
           </div>
           <div className="mb-3">
               <label htmlFor="email" className="form-label">Email</label>
-              <input type="email"  className="form-control" id="email"  placeholder="Name@email.com" />
+              <input type="email" onChange={this.handleAdd} className="form-control" name="email" id="email"  placeholder="Name@email.com" />
           </div>
           <div className="mb-3">
               <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" className="form-control" id="password"  placeholder="Password" />
+              <input type="password" onChange={this.handleAdd}  className="form-control" name="password" id="password"  placeholder="Password" />
           </div>
           <div className="d-grid gap-2">
             <input type="submit" onClick={this.handleSubmit} name="button" className="btn btn-success btn-lg" id="submit" value="Submit"></input>
